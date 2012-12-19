2922eede29b01b4f25b80264779a180c223d4722
Thorsten Zerha <thorsten.zerha@tastenwerk.com>
2012-12-19 18:36:22 +0100

    inter-tags module improved

--- public/javascripts/widgets/inter-tags.js
+++ public/javascripts/widgets/inter-tags.js
@@ -10,16 +10,34 @@ $(function(){
 
     var self = this;
 
-
-    self.transformTag = function transformTag( val ){
-      $(self).find('.tags').append('<span class="inter-tag">'+val+'</span>');
+    function appendTag( tag ){
+      $(self).before($('<span/>').append($('<span/>').addClass('tag-content').text(tag)).addClass('tag')
+                      .append($('<span/>').addClass('remove-tag live-tipsy').html('&times;')
+                        .attr('original-title', 'Diesen Tag löschen')));
     }
 
-    if( $(self).val().length ){
-      $(self).val().split(', ').each( function(){
-        transformTag( this );
-      })
-    }
+    if( $(self).hasClass('taggified') )
+      return;
+
+    $(self).addClass('taggified');
+
+    var tags = $(self).val().split( options.separator || ',' );
+    for( var i=0, tag; tag=tags[i]; i++ )
+      appendTag( tag );
+
+    $(self).val('');
+
+    $(self).on('keydown', function(e){
+      if( e.keyCode === 13 ){ // enter
+        appendTag( $(this).val() );
+        $(this).val('');
+      }
+    })
+    $(self).parent().parent().on('click','.remove-tag', function(e){
+      $(this).closest('.tag').remove();
+      e.preventDefault();
+      e.stopPropagation();
+    });
 
   };
 
